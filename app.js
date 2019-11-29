3f2d2c2b1323b01423dcf372e6b521d2f0ad098e
杨裕则 <yangyuze1018@hotmail.com>
2019-11-30 01:06:10 +1100

    add port

--- app.js
+++ app.js
@@ -61,6 +61,6 @@ app.use('/', indexRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
 	console.log('Server is running!!!');
 });
